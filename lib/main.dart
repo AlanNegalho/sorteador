62dcd5396421947909d8feda9f0dafce347707f9
AlanNegalho <alandevads@gmail.com>
2026-03-26 21:32:18 -0300

    create input user and card value

--- lib/main.dart
+++ lib/main.dart
@@ -59,11 +59,99 @@ class PublicScreen extends StatelessWidget {
           ],
         ),
       ),
-      body: const Center(
-        child: Text(
-          'Body',
-          style: TextStyle(fontSize: 24),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/fundo.png'),
+            fit: BoxFit.fitHeight,
+          ),
         ),
+        child: Stack(children: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Text(
+                          '📝 Número máximo:',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.orange[700],
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        // Campo de entrada para o número máximo
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Card(
+                                child: TextField(
+                                  keyboardType: TextInputType.number,
+                                  inputFormatters: [
+                                    FilteringTextInputFormatter.digitsOnly,
+                                  ],
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                    labelText: 'Ex: 100, 250,..',
+                                    prefixText: '1 a ',
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+
+                  // 🔢 DISPLAY
+                  Container(
+                    width: 200,
+                    height: 200,
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(25),
+                      boxShadow: const [
+                        BoxShadow(
+                          blurRadius: 25,
+                          color: Colors.black26,
+                        )
+                      ],
+                    ),
+                    child: const Center(
+                      child: Text(
+                        '0', // ← Número sorteado)
+
+                        style: TextStyle(
+                            fontSize: 90,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                            shadows: [
+                              Shadow(
+                                blurRadius: 25,
+                                color: Colors.greenAccent,
+                              )
+                            ]),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
